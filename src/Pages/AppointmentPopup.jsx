3019396a17894a04e4d57fd0a045713a4cb23321
Ranashekhar007 <ranashekhar007@gmail.com>
2023-06-20 13:20:47 +0530

    code .

--- src/Pages/AppointmentPopup.jsx
+++ src/Pages/AppointmentPopup.jsx
@@ -1,32 +1,46 @@
 import React, { useState } from 'react'
 
-const AppointmentPopup = ({ data }) => {
-    const [setshowmodal, setSetshowmodal] = useState(data)
+const AppointmentPopup = () => {
+const [patient, setpatient]= useState("")
+const [department, setdepartment]= useState("")
+const [message, setmessage]= useState("")
+const [time, settime]= useState("")
+const [date, setdate]= useState("")
+const [phone, setphone]= useState("")
+const [email, setemail]= useState("")
+const [doctor, setdoctor]= useState("")
+
+
+
+
+
+
+console.log(date);
+
     const handleModal = () => {
         setSetshowmodal(false)
     }
     return (
-        <div class="modal fade"   id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" >
-          <div class="modal-content" >
-            <div class="modal-header"    >
-              <h1 class="modal-title fs-5" id="exampleModalLabel"> Book an Appointment</h1>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                            
-            <form>
+        <div class="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" >
+                <div class="modal-content" >
+                    <div class="modal-header"    >
+                        <h1 class="modal-title fs-5" id="exampleModalLabel"> Book an Appointment</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form>
                             <div className="row">
                                 <div className="col-lg-12">
                                     <div className="mb-3">
                                         <label className="form-label">Patient Name <span className="text-danger">*</span></label>
-                                        <input name="name" id="name" type="text" className="form-control" placeholder="Patient Name :" />
+                                        <input name="name" id="name" onChange={(e)=> setpatient(e.target.value)} type="text" className="form-control" placeholder="Patient Name :"  />
                                     </div>
                                 </div>
                                 <div className="col-lg-4 col-md-6">
                                     <div className="mb-3">
                                         <label className="form-label">Departments</label>
-                                        <select className="form-select form-control">
+                                        <select className="form-select form-control" onChange={(e)=> setdepartment(e.target.value)}>
                                             <option value="EY">Eye Care</option>
                                             <option value="GY">Gynecologist</option>
                                             <option value="PS">Psychotherapist</option>
@@ -41,7 +55,7 @@ const AppointmentPopup = ({ data }) => {
                                 <div className="col-lg-4 col-md-6">
                                     <div className="mb-3">
                                         <label className="form-label">Doctor</label>
-                                        <select className="form-select form-control">
+                                        <select className="form-select form-control" onChange={(e)=>setdoctor(e.target.value)}>
                                             <option value="CA">Dr. Calvin Carlo</option>
                                             <option value="CR">Dr. Cristino Murphy</option>
                                             <option value="AL">Dr. Alia Reddy</option>
@@ -56,56 +70,34 @@ const AppointmentPopup = ({ data }) => {
                                 <div className="col-lg-4 col-md-6">
                                     <div className="mb-3">
                                         <label className="form-label">Your Email <span className="text-danger">*</span></label>
-                                        <input name="email" id="email" type="email" className="form-control" placeholder="Your email :" />
+                                        <input name="email" id="email" type="email" onChange={(e)=>setemail(e.target.value)}className="form-control" placeholder="Your email :" />
                                     </div>
                                 </div>
                                 <div className="col-lg-4 col-md-6">
                                     <div className="mb-3">
                                         <label className="form-label">Your Phone <span className="text-danger">*</span></label>
-                                        <input name="phone" id="phone" type="tel" className="form-control" placeholder="Your Phone :" />
+                                        <input name="phone" id="phone" type="tel" className="form-control" placeholder="Your Phone :" onChange={(e)=>setphone(e.target.value)}/>
                                     </div>
                                 </div>
                                 <div className="col-lg-4 col-md-6">
                                     <div className="mb-3" style={{ position: 'relative' }}>
                                         <label className="form-label">Date :</label>
-                                        <input name="date" type="text" className="form-control start" placeholder="Select date :" />
+                                        <input name="date" type="date" className="form-control start" placeholder="Select date :" onChange={(e)=>setdate(e.target.value)} />
                                         <div className="qs-datepicker-container qs-hidden">
-                                            <div className="qs-datepicker">
-                                                <div className="qs-controls">
-                                                    <div className="qs-arrow qs-left"></div>
-                                                    <div className="qs-month-year">
-                                                        <span className="qs-month">June</span>
-                                                        <span className="qs-year">2023</span>
-                                                    </div>
-                                                    <div className="qs-arrow qs-right"></div>
-                                                </div>
-                                                <div className="qs-squares">
-                                                    {/* Calendar days */}
-                                                </div>
-                                                <div className="qs-overlay qs-hidden">
-                                                    <div>
-                                                        <input className="qs-overlay-year" placeholder="4-digit year" inputMode="numeric" />
-                                                        <div className="qs-close">✕</div>
-                                                    </div>
-                                                    <div className="qs-overlay-month-container">
-                                                        {/* Calendar overlay months */}
-                                                    </div>
-                                                    <div className="qs-submit qs-disabled">Submit</div>
-                                                </div>
-                                            </div>
+
                                         </div>
                                     </div>
                                 </div>
                                 <div className="col-lg-4 col-md-6">
                                     <div className="mb-3">
                                         <label className="form-label" htmlFor="input-time">Time :</label>
-                                        <input name="time" type="text" className="form-control timepicker" id="input-time" placeholder="03:30 PM" />
+                                        <input name="time" type="text" className="form-control timepicker" id="input-time" placeholder="03:30 PM" onChange={(e)=>settime(e.target.value)} />
                                     </div>
                                 </div>
                                 <div className="col-lg-12">
                                     <div className="mb-3">
                                         <label className="form-label">Comments <span className="text-danger">*</span></label>
-                                        <textarea name="comments" id="comments" rows="4" className="form-control" placeholder="Your Message :"></textarea>
+                                        <textarea name="comments" id="comments" rows="4" className="form-control" placeholder="Your Message :" onChange={(e)=>setmessage(e.target.value)}></textarea>
                                     </div>
                                 </div>
                                 <div className="col-lg-12">
@@ -115,15 +107,12 @@ const AppointmentPopup = ({ data }) => {
                                 </div>
                             </div>
                         </form>
+                    </div>
+
+                </div>
             </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-          </div>
         </div>
-      </div>
-            )
+    )
 }
 
-            export default AppointmentPopup
+export default AppointmentPopup
